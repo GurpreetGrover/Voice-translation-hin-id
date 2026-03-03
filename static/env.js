44827128d1d7aa717932110bbd4d4e485e26fbfe
Gurpreet Singh <gurpreet.grover01@gmail.com>
2026-03-03 13:47:52 +0530

    change in frontend: exported the backend url from index.html to env.js

---
+++ static/env.js
@@ -0,0 +1,9 @@
+// env.js
+// Store your frontend environment variables here.
+// Make sure to add this file to your .gitignore so it isn't tracked in version control.
+
+window.ENV = {
+    // Change this to your deployed URL when pushing to production
+    // e.g., "https://your-fastapi-app.onrender.com/translate-audio"
+    BACKEND_URL: "https://voic-bridge-2zof3.ondigitalocean.app/translate-audio"
+};
